9a1f3edb5300086b8a43e460636d93c2cd6c234e
Dan Iverson <dan.iverson@innovativeartects.com>
2020-06-21 02:07:20 -0400

    Got Service Pricipal with cert created.

--- execution/Script/New-ResourceGroups.ps1
+++ execution/Script/New-ResourceGroups.ps1
@@ -1,4 +1,5 @@
 
+C:\01servers\danspersonalportal\cert\Connect-DansPP.ps1
 
-New-AzResourceGroup -
-
+New-AzResourceGroup 
+Get-LPAccounts | Where-Object { $_.Username -match "hotmail" } | Where { $_.name -match "" }
--- execution/source/cert/New-AzServicePricipalAccount.ps1
+++ execution/source/cert/New-AzServicePricipalAccount.ps1
@@ -11,6 +11,10 @@ $SPDisplayName = 'danspersonalportal'
 $cnstr = 'CN=' + $CNName 
 $keyspec = 'Exchange'
 $StoreLocation = 'CurrentUser'
+$SubscriptionName = 'DansPersonalPortal'
+$ApplicationId = '54b6a397-5a22-4c27-8fc1-df1d4efd63e6'
+$TenantId = 'a0d5c447-c26a-4341-ade5-04d8615fc1e7'
+
 
 (Get-AzSubscription -SubscriptionName "Contoso Default").TenantId
 
@@ -29,7 +33,7 @@ if ( $PSVersionTable.PSEdition -eq 'Desktop' ) {
 
 } elseif ( $PSVersionTable.PSEdition -eq 'Core' ) {
     # Only run if you could not use New-SelfSignedCertificate
-    Import-Module -Name C:\vsts\IA\dji-terraform-example\New-SelfSignedCertificateEx.ps1
+    Import-Module -Name C:\vsts\IA\dji-terraform-example\execution\source\cert\New-SelfSignedCertificateEx.ps1
 
     $SSCertParam = @{
         StoreLocation = $StoreLocation
@@ -40,18 +44,33 @@ if ( $PSVersionTable.PSEdition -eq 'Desktop' ) {
 
     New-SelfSignedCertificateEx @SSCertParam
 
-    $cert = Get-ChildItem -path Cert:\CurrentUser\my | Where-Object {$PSitem.Subject -eq $cnstr }
-
+    $cert = Get-ChildItem -path Cert:\CurrentUser\my | Where-Object {$_.Subject -match $SPDisplayName }
+    $cert 
 }
 
+$keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
 
-$TenantId = (Get-AzSubscription -SubscriptionName "Contoso Default").TenantId
-$ApplicationId = (Get-AzADApplication -DisplayNameStartWith exampleapp).ApplicationId
+$sp = New-AzADServicePrincipal -DisplayName $SPDisplayName -CertValue $keyValue -EndDate $cert.NotAfter -StartDate $cert.NotBefore
+Sleep 20
+New-AzRoleAssignment -RoleDefinitionName Owner -ServicePrincipalName $sp.ApplicationId
 
- $Thumbprint = (Get-ChildItem cert:\CurrentUser\My\ | Where-Object {$_.Subject -eq "CN=exampleappScriptCert" }).Thumbprint
- Connect-AzAccount -ServicePrincipal `
-  -CertificateThumbprint $Thumbprint `
-  -ApplicationId $ApplicationId `
-  -TenantId $TenantId
+$TenantId = (Get-AzSubscription -SubscriptionName $SPDisplayName).TenantId
+$ApplicationId = (Get-AzADApplication -DisplayNameStartWith $SPDisplayName).ApplicationId
 
-######################################################################################
+
+
+ $Thumbprint = (Get-ChildItem cert:\CurrentUser\My\ | Where-Object {$_.Subject -eq $cnstr }).Thumbprint
+ Connect-AzAccount -ServicePrincipal -CertificateThumbprint $Thumbprint -ApplicationId $ApplicationId -TenantId $TenantId
+
+
+
+# $TenantId = (Get-AzSubscription -SubscriptionName $SubscriptionName).TenantId
+# $ApplicationId = (Get-AzADApplication -DisplayNameStartWith exampleapp).ApplicationId
+
+#  $Thumbprint = (Get-ChildItem cert:\CurrentUser\My\ | Where-Object {$_.Subject -eq "CN=exampleappScriptCert" }).Thumbprint
+#  Connect-AzAccount -ServicePrincipal `
+#   -CertificateThumbprint $Thumbprint `
+#   -ApplicationId $ApplicationId `
+#   -TenantId $TenantId
+
+
