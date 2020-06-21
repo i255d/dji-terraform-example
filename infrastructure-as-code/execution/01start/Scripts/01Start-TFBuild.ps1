74a360f0687b1f3af25ef52b6e4d2c1f1b252c53
Dan Iverson <dan.iverson@innovativeartects.com>
2020-06-21 15:19:18 -0400

    Added logic for login

--- infrastructure-as-code/execution/01start/Scripts/01Start-TFBuild.ps1
+++ infrastructure-as-code/execution/01start/Scripts/01Start-TFBuild.ps1
@@ -1,8 +1,17 @@
-$WorkingPath = 'C:\vsts\IA\dji-terraform-example\execution\01start\tf'
+$SubscriptionName = 'DansPersonalPortal'
+$WorkingPath = 'C:\vsts\IA\dji-terraform-example\infrastructure-as-code\execution\01start\tf'
+$VerbosePreference = 'continue'
+if ( Test-Path -Path $WorkingPath ) {
+    Set-Location -Path $WorkingPath
+} else {
+    Write-Verbose -Message "Unable to fine `$WorkingPath"
+}
 
-Set-Location -Path $WorkingPath
-
-
-
+$Context = Get-AzContext
+if ( -not ($Context.Subscription.Name -eq $SubscriptionName) ) {
+    C:\01servers\danspersonalportal\cert\Connect-DansPP.ps1
+} else {
+    Write-Verbose -Message "Context -eq $SubscriptionName"
+}
 
 
